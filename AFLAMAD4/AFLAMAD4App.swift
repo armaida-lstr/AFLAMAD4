90a52f78afb8c8e2b5003d3b2d3ca90c5c82ca87
armaidalstr <armaidalstr@gmail.com>
2022-06-02 20:58:59 +0700

    Initial Commit

---
+++ AFLAMAD4/AFLAMAD4App.swift
@@ -0,0 +1,17 @@
+//
+//  AFLAMAD4App.swift
+//  AFLAMAD4
+//
+//  Created by MacBook Pro on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AFLAMAD4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
